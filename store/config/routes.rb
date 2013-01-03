6da7839fb6041968031b845d56dfb25d6a5c8d79
David Silver <dsilver@candidatemetrics.com>
2013-01-02 16:13:00 -0800

    set up pages

--- store/config/routes.rb
+++ store/config/routes.rb
@@ -1,8 +1,10 @@
 Store::Application.routes.draw do
-  %w[about privacy license].each do |page|
-    get page, controller: 'info', action: page
-  end
+  #%w[about privacy license].each do |page|
+  #  get page, controller: 'info', action: page
+  #end
 
+  resources :pages, except: :show
   resources :products
   root to: 'products#index'
+  get ':id', to: 'pages#show', as: :page
 end
