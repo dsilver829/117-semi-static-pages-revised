a92f308ac47591674b6a43dbafa3343ecb63dbfd
David Silver <dsilver@candidatemetrics.com>
2013-01-02 16:08:01 -0800

    set up static pages

--- store/config/routes.rb
+++ store/config/routes.rb
@@ -1,4 +1,8 @@
 Store::Application.routes.draw do
+  %w[about privacy license].each do |page|
+    get page, controller: 'info', action: page
+  end
+
   resources :products
   root to: 'products#index'
 end
